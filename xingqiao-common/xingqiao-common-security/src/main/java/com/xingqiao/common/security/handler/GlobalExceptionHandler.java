066f816228d4a44e17ff4822460fd11acf39b004
qinguanmu <759525964@qq.com>
2025-09-13 01:09:41 +0800

    客户端kyc

--- xingqiao-common/xingqiao-common-security/src/main/java/com/xingqiao/common/security/handler/GlobalExceptionHandler.java
+++ xingqiao-common/xingqiao-common-security/src/main/java/com/xingqiao/common/security/handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.xingqiao.common.security.handler;
 
 import javax.servlet.http.HttpServletRequest;
+
+import com.xingqiao.common.core.domain.R;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.BindException;
@@ -140,11 +142,11 @@ public class GlobalExceptionHandler
      * 自定义验证异常
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e)
+    public R handleMethodArgumentNotValidException(MethodArgumentNotValidException e)
     {
         log.error(e.getMessage(), e);
         String message = e.getBindingResult().getFieldError().getDefaultMessage();
-        return AjaxResult.error(message);
+        return R.fail(message);
     }
 
     /**
